9be623ebf9d8784e9f4c71575acecd80572ffec1
Russell Greene <russellgreene8@gmail.com>
2017-01-01 22:34:42 -0700

    Add appveyor

--- build.py
+++ build.py
@@ -4,6 +4,18 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     
-    builder.add_common_builds()
+    if platform.system() == "Windows":
+         for shared in [True, False]:
+            for build_type in ["Debug", "Release"]:
+                for vers in builder.visual_versions:
+                    for runtimes in builder.visual_runtimes:
+                        builder.add({"arch": "x86_64",
+                                     "build_type": build_type,
+                                     "compiler": "Visual Studio",
+                                     "compiler.version": vers,
+                                     "compiler.runtime": runtimes},
+                                    {"llvm:shared": shared})
+    else:
+        builder.add_common_builds()
     
     builder.run()
